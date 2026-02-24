e6ea09b9af8a88a886770f88742e7b9d3850d715
Moritz Nossek <moritz.nossek@googlemail.com>
2026-02-24 20:26:46 +0100

    Add new plugins to init.lua configuration

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,27 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+  {
+    'nvzone/typr',
+    dependencies = 'nvzone/volt',
+    opts = {},
+    cmd = { 'Typr', 'TyprStats' },
+  },
+  {
+    'm4xshen/hardtime.nvim',
+    lazy = false,
+    dependencies = { 'MunifTanjim/nui.nvim' },
+    opts = {},
+  },
+  {
+    'rcarriga/nvim-notify',
+    config = function()
+      require('notify').setup {
+        timeout = 3000,
+        top_down = true,
+      }
+      vim.notify = require 'notify'
+    end,
+  },
+}
